e4f11010d3f24ab44dd7f9ab5599d3ac640060b7
sfernandez <sfdezlop@gmail.com>
2023-04-19 22:42:35 +0200

    Add create method at productmovements controller

--- src/controllers/productmovements.controller.ts
+++ src/controllers/productmovements.controller.ts
@@ -3,7 +3,7 @@ import createDebug from 'debug';
 
 import { HTTPError } from '../interfaces/error.js';
 import { ProductMovementMongoRepo } from '../repositories/productmovements.mongo.repo';
-const debug = createDebug('ERP:controller:users');
+const debug = createDebug('ERP:controller:productmovements');
 export class ProductMovementsController {
   constructor(public repo: ProductMovementMongoRepo) {
     debug('Instantiate');
@@ -16,7 +16,25 @@ export class ProductMovementsController {
         results: data,
       });
     } catch (error) {
+      next(error);
       throw new HTTPError(400, 'analytics Not found', 'analytics Not found');
+    }
+  }
+
+  async create(req: Request, resp: Response, next: NextFunction) {
+    try {
+      debug('create:post');
+      if (!req.body) throw new HTTPError(404, 'Body needed', 'Body needed');
+      const data = await this.repo.create(req.body);
+      resp.json({
+        results: data,
+      });
+    } catch (error) {
+      throw new HTTPError(
+        400,
+        'Creation is not possible',
+        'Creation is not possible'
+      );
       next(error);
     }
   }
@@ -32,13 +50,12 @@ export class ProductMovementsController {
         results: data,
       });
     } catch (error) {
-      throw new HTTPError(400, 'Gallery Not found', 'Gallery Not found');
       next(error);
+      throw new HTTPError(400, 'Gallery Not found', 'Gallery Not found');
     }
   }
 
   async getById(req: Request, resp: Response, next: NextFunction) {
-    debug('entrando en el controller');
     try {
       const getId = req.params.id;
       const data = await this.repo.queryId(getId);
@@ -47,6 +64,7 @@ export class ProductMovementsController {
       });
     } catch (error) {
       next(error);
+      throw new HTTPError(400, 'Record Not found', 'Record Not found');
     }
   }
 
@@ -60,6 +78,11 @@ export class ProductMovementsController {
       });
     } catch (error) {
       next(error);
+      throw new HTTPError(
+        400,
+        'Count is not possible',
+        'Count is not possible'
+      );
     }
   }
 
@@ -72,6 +95,45 @@ export class ProductMovementsController {
       });
     } catch (error) {
       next(error);
+      throw new HTTPError(
+        400,
+        'Count is not possible',
+        'Count is not possible'
+      );
+    }
+  }
+
+  async stockBySku(req: Request, resp: Response, next: NextFunction) {
+    try {
+      debug('stockBySku-method');
+      const data = await this.repo.stockBySku(req.params.id);
+      resp.json({
+        results: data,
+      });
+    } catch (error) {
+      next(error);
+      throw new HTTPError(
+        400,
+        'Calculation of stock by sku is not possible',
+        'Calculation of stock by sku is not possible'
+      );
+    }
+  }
+
+  async stock(req: Request, resp: Response, next: NextFunction) {
+    try {
+      debug('stock-method');
+      const data = await this.repo.stock();
+      resp.json({
+        results: data,
+      });
+    } catch (error) {
+      next(error);
+      throw new HTTPError(
+        400,
+        'Calculation of stock is not possible',
+        'Calculation of stock is not possible'
+      );
     }
   }
 }
