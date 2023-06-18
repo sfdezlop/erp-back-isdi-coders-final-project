8164b6765c315efafe6386209e32e1d41a3d9195
sfernandez <sfdezlop@gmail.com>
2023-06-18 16:31:04 +0200

    Remove code smells

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
-import jwt from 'jsonwebtoken';
 import path from 'path';
-import { __dirname, config } from './config.js';
+import { __dirname } from './config.js';
 import express, { NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
@@ -11,7 +10,6 @@ import { productsRouter } from './routers/products.router.js';
 import { productMovementsRouter } from './routers/productmovements.router.js';
 import fs from 'fs';
 import { collectionsRouter } from './routers/collections.router.js';
-import { Auth, PayloadToken } from './services/auth.js';
 
 const debug = createDebug('ERP:app');
 export const app = express();
--- src/repositories/collections.mongo.repo.test.ts
+++ src/repositories/collections.mongo.repo.test.ts
@@ -1,14 +1,5 @@
 import { CollectionsMongoRepo } from './collections.mongo.repo.js';
-import mongoose, { Model } from 'mongoose';
 // Import { ProductModel } from './products.mongo.model';
-
-const mockExecFunction = (mockValue: unknown) => ({
-  exec: jest.fn().mockResolvedValue(mockValue),
-});
-
-const ProductModel = {
-  aggregate: jest.fn(),
-};
 
 describe('Given a new CollectionsMongoRepo created with a public static function (to follow singleton patron)', () => {
   const instanceOfCollectionsMongoRepo = CollectionsMongoRepo.getInstance();
